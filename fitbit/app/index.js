1bc5790a7680d338e2a32b3ca5c5055a0e21917b
Calum Sieppert <sieppertcalum@gmail.com>
2020-04-29 10:01:39 -0600

    Collect accelerometer data on Fitbit

--- fitbit/app/index.js
+++ fitbit/app/index.js
@@ -2,35 +2,48 @@ import document from 'document'
 import { peerSocket } from 'messaging'
 import { handlePSError } from '../shared/handlePSError'
 import { sendMessage } from '../shared/sendMessage'
-import { Accelerometer } from "accelerometer";
+import { Accelerometer } from 'accelerometer'
+import { abs, mean } from 'scientific'
 
-if (Accelerometer) {
-  // 30 readings per second, 60 readings per batch
-  // the callback will be called once every two seconds
-  const accel = new Accelerometer({ frequency: 30, batch: 60 });
-  accel.addEventListener("reading", () => {
-    for (let index = 0; index < accel.readings.timestamp.length; index++) {
-      console.log(
-          `Accelerometer Reading: \
-          timestamp=${accel.readings.timestamp[index]}, \
-          [${accel.readings.x[index]}, \
-          ${accel.readings.y[index]}, \
-          ${accel.readings.z[index]}]`
-      );
-    }
-  })
-  accel.start();
+function absMean(f32Array) {
+    return mean(abs(f32Array))
 }
 
+function roundToNearestSecond(date) {
+    date.setSeconds(date.getSeconds() + Math.round(date.getMilliseconds() / 1000), 0)
+    return date
+}
+
+if (Accelerometer) {
+    const freq = 30
+    const accel = new Accelerometer({ frequency: freq, batch: freq })
+    let prevSecond = -1
+    accel.addEventListener('reading', () => {
+        const { x, y, z } = accel.readings
+        const date = roundToNearestSecond(new Date())
+        const currentSecond = date.getSeconds()
+        if (prevSecond === -1 || prevSecond !== currentSecond) {
+            prevSecond = currentSecond
+            const processed = {
+                x: absMean(x),
+                y: absMean(y),
+                z: absMean(z),
+                time: date.getTime()
+            }
+            sendMessage(processed)
+        }
+    })
+    accel.start()
+}
 const statusText = document.getElementById('status')
 statusText.text = 'Sending to server...'
 
 peerSocket.onopen = function () {
-  sendMessage('Hello server!')
+    // sendMessage('Hello server!')
 }
 
 peerSocket.onmessage = function (evt) {
-  statusText.text = 'Server echo: ' + JSON.stringify(evt.data)
+    statusText.text = 'Server echo: ' + JSON.stringify(evt.data)
 }
 
 peerSocket.onerror = handlePSError
--- fitbit/companion/index.js
+++ fitbit/companion/index.js
@@ -5,14 +5,22 @@ import { ServerConnection } from './ServerConnection'
 const server = new ServerConnection()
 
 peerSocket.onopen = function () {
-  console.log('Device connected')
+    console.log('Device connected')
 }
 
 peerSocket.onmessage = function (evt) {
-  const { data } = evt
-  console.log('Message from device: ', data)
-  console.log('Sending to server...')
-  server.send(data)
+    const {data} = evt
+    console.log('Message from device: ')
+    const date = new Date(data.time)
+    console.log(data.x, data.y, data.z, date.toString())
+    // console.log(data.timestamp)
+    // console.log(data.x)
+    // data.timestamp.forEach((time) => console.log('TIME', time))
+    // data.x.forEach((x) => console.log('X', x))
+    // localStorage.clear()
+    // const count = localStorage.getItem('count') || 0;
+    // localStorage.setItem('count', count + 1);
+    // console.log(localStorage.getItem('count'));
 }
 
 peerSocket.onerror = handlePSError
